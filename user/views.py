e6cef167cb3ae90eeb6c89188b0a9bf85c02d820
Sandy <sandychen123456789@gmail.com>
2024-03-14 21:44:23 +0800

    完成user註冊表單POST回傳值得取得(前後端運用)

--- user/views.py
+++ user/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 def user_register(request):
-    return render(request, "user/register.html")  # 因為有兩層目錄所以要這樣寫
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        username = request.POST.get("username")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+        print(username, password1, password2)
+
+    return render(
+        request, "user/register.html", {"form": UserCreationForm()}
+    )  # 因為有兩層目錄所以要這樣寫
